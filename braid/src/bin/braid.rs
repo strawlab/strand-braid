fd415eef91d061653f54aebd11194851fb9a034d
Andrew Straw <strawman@astraw.com>
2021-01-03 13:33:49 +0100

    braid: docstrings and comment

--- braid/src/bin/braid.rs
+++ braid/src/bin/braid.rs
@@ -8,13 +8,19 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "braid", about = "multi-camera realtime 3D tracker")]
 struct BraidLauncherCliArgs {
+    /// Command to execute (e.g. run, show-config, default-config)
     command: String,
+    /// Options specific to the command
     options: Vec<String>,
 }
 
 fn main() -> Result<()> {
     env_tracing_logger::init();
     // braid::braid_start("braid")?;
+
+    // TODO: In case of no command given (or a query command), iterate all dirs
+    // on environment path, collect braid-* executables, show these as possible
+    // commands.
 
     let args = BraidLauncherCliArgs::from_args();
     debug!("{:?}", args);
