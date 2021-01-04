bec10e2d51deed8bfb85c8fc0760329a39ba897b
Andrew Straw <strawman@astraw.com>
2021-01-04 22:55:50 +0100

    writing changelog with ubuntu release

--- braid/src/bin/write-debian-changelog.rs
+++ braid/src/bin/write-debian-changelog.rs
@@ -1,5 +1,7 @@
 fn main() {
-    let cli_arg1 = std::env::args().skip(1).next().unwrap();
+    let mut args = std::env::args().skip(1);
+    let cli_arg1 = args.next().unwrap();
+    let cli_arg2 = args.next().unwrap();
     let date_r = std::process::Command::new("sh")
         .arg("-c")
         .arg("date -R")
@@ -8,13 +10,14 @@ fn main() {
 
     let date_r = String::from_utf8_lossy(&date_r.stdout);
     let s = format!(
-        "{} ({}-1) xenial; urgency=low
+        "{} ({}-1) {}; urgency=low
 
   * New release
 
  -- Andrew Straw <strawman@astraw.com>  {}",
         cli_arg1,
         env!("CARGO_PKG_VERSION"),
+        cli_arg2,
         date_r
     );
     println!("{}", s);
