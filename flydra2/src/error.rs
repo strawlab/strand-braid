50394c19b0ec11f722a5776680bb31426d5018ad
Andrew Straw <strawman@astraw.com>
2021-04-08 09:43:44 +0200

    flydra2: move Error to own module

---
+++ flydra2/src/error.rs
@@ -0,0 +1,125 @@
+#[cfg(feature = "backtrace")]
+use std::backtrace::Backtrace;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("{source}")]
+    FlydraTypes {
+        #[from]
+        source: flydra_types::FlydraTypesError,
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace,
+    },
+    #[error("{source}")]
+    Mvg {
+        #[from]
+        source: mvg::MvgError,
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace,
+    },
+    #[error("{source}")]
+    Io {
+        #[from]
+        source: std::io::Error,
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace,
+    },
+    #[error("{source}")]
+    Csv {
+        #[from]
+        source: csv::Error,
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace,
+    },
+    #[error("{source}")]
+    GetTimezone {
+        #[from]
+        source: iana_time_zone::GetTimezoneError,
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace,
+    },
+    #[error("{source}")]
+    SerdeJson {
+        #[from]
+        source: serde_json::Error,
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace,
+    },
+    #[error("{source}")]
+    SerdeYaml {
+        #[from]
+        source: serde_yaml::Error,
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace,
+    },
+    #[error("{source}")]
+    FuturesSendError {
+        #[from]
+        source: futures::channel::mpsc::SendError,
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace,
+    },
+    #[error("{source}")]
+    TomlSerError {
+        #[from]
+        source: toml::ser::Error,
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace,
+    },
+    #[error("{source}")]
+    TomlDeError {
+        #[from]
+        source: toml::de::Error,
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace,
+    },
+    #[error("invalid hypothesis testing parameters")]
+    InvalidHypothesisTestingParameters,
+    #[error("insufficient data to calculate FPS")]
+    InsufficientDataToCalculateFps,
+    #[error("{source}")]
+    ZipDir {
+        #[from]
+        source: zip_or_dir::Error,
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace,
+    },
+    #[error("Error opening {filename}: {source}")]
+    FileError {
+        what: &'static str,
+        filename: String,
+        source: Box<dyn std::error::Error + Sync + Send>,
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace,
+    },
+    #[error("{source}")]
+    WrappedError {
+        source: Box<dyn std::error::Error + Sync + Send>,
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace,
+    },
+}
+
+pub fn file_error<E>(what: &'static str, filename: String, source: E) -> Error
+where
+    E: 'static + std::error::Error + Sync + Send,
+{
+    Error::FileError {
+        what,
+        filename,
+        source: Box::new(source),
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace::capture(),
+    }
+}
+
+pub fn wrap_error<E>(source: E) -> Error
+where
+    E: 'static + std::error::Error + Sync + Send,
+{
+    Error::WrappedError {
+        source: Box::new(source),
+        #[cfg(feature = "backtrace")]
+        backtrace: Backtrace::capture(),
+    }
+}
--- flydra2/src/flydra2.rs
+++ flydra2/src/flydra2.rs
@@ -1,10 +1,5 @@
 #![cfg_attr(feature = "backtrace", feature(backtrace))]
 
-#[cfg(feature = "backtrace")]
-use std::backtrace::Backtrace;
-
-use thiserror::Error;
-
 use log::{debug, error, info, trace};
 
 use serde::{Deserialize, Serialize};
@@ -87,130 +82,10 @@ pub use crate::frame_bundler::StreamItem;
 
 pub type MyFloat = flydra_types::MyFloat;
 
+mod error;
+pub use error::{file_error, wrap_error, Error};
+
 pub type Result<M> = std::result::Result<M, Error>;
-
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error("{source}")]
-    FlydraTypes {
-        #[from]
-        source: flydra_types::FlydraTypesError,
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace,
-    },
-    #[error("{source}")]
-    Mvg {
-        #[from]
-        source: mvg::MvgError,
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace,
-    },
-    #[error("{source}")]
-    Io {
-        #[from]
-        source: std::io::Error,
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace,
-    },
-    #[error("{source}")]
-    Csv {
-        #[from]
-        source: csv::Error,
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace,
-    },
-    #[error("{source}")]
-    GetTimezone {
-        #[from]
-        source: iana_time_zone::GetTimezoneError,
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace,
-    },
-    #[error("{source}")]
-    SerdeJson {
-        #[from]
-        source: serde_json::Error,
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace,
-    },
-    #[error("{source}")]
-    SerdeYaml {
-        #[from]
-        source: serde_yaml::Error,
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace,
-    },
-    #[error("{source}")]
-    FuturesSendError {
-        #[from]
-        source: futures::channel::mpsc::SendError,
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace,
-    },
-    #[error("{source}")]
-    TomlSerError {
-        #[from]
-        source: toml::ser::Error,
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace,
-    },
-    #[error("{source}")]
-    TomlDeError {
-        #[from]
-        source: toml::de::Error,
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace,
-    },
-    #[error("invalid hypothesis testing parameters")]
-    InvalidHypothesisTestingParameters,
-    #[error("insufficient data to calculate FPS")]
-    InsufficientDataToCalculateFps,
-    #[error("{source}")]
-    ZipDir {
-        #[from]
-        source: zip_or_dir::Error,
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace,
-    },
-    #[error("Error opening {filename}: {source}")]
-    FileError {
-        what: &'static str,
-        filename: String,
-        source: Box<dyn std::error::Error + Sync + Send>,
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace,
-    },
-    #[error("{source}")]
-    WrappedError {
-        source: Box<dyn std::error::Error + Sync + Send>,
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace,
-    },
-}
-
-pub fn file_error<E>(what: &'static str, filename: String, source: E) -> Error
-where
-    E: 'static + std::error::Error + Sync + Send,
-{
-    Error::FileError {
-        what,
-        filename,
-        source: Box::new(source),
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace::capture(),
-    }
-}
-
-pub fn wrap_error<E>(source: E) -> Error
-where
-    E: 'static + std::error::Error + Sync + Send,
-{
-    Error::WrappedError {
-        source: Box::new(source),
-        #[cfg(feature = "backtrace")]
-        backtrace: Backtrace::capture(),
-    }
-}
 
 pub(crate) fn generate_observation_model<R>(
     cam: &flydra_mvg::MultiCamera<R>,
