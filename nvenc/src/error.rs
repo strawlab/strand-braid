9250888fbca72a70cbcc3adf557cae5ebd9ae2d6
Andrew Straw <strawman@astraw.com>
2021-06-12 11:00:43 +0200

    nvenc: errors include backtrace

--- nvenc/src/error.rs
+++ nvenc/src/error.rs
@@ -3,21 +3,17 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum NvEncError {
     #[error("dynlink-cuda returned error `{0}`")]
-    DynlinkCudaError(dynlink_cuda::CudaError),
+    DynlinkCudaError(
+        #[from]
+        #[cfg_attr(feature = "backtrace", backtrace)]
+        dynlink_cuda::CudaError,
+    ),
     #[error("dynlink-nvidia-encode returned error `{0}`")]
-    DynlinkNvidiaEncodeError(dynlink_nvidia_encode::NvencError),
-}
-
-impl From<dynlink_cuda::CudaError> for NvEncError {
-    fn from(orig: dynlink_cuda::CudaError) -> NvEncError {
-        NvEncError::DynlinkCudaError(orig)
-    }
-}
-
-impl From<dynlink_nvidia_encode::NvencError> for NvEncError {
-    fn from(orig: dynlink_nvidia_encode::NvencError) -> NvEncError {
-        NvEncError::DynlinkNvidiaEncodeError(orig)
-    }
+    DynlinkNvidiaEncodeError(
+        #[from]
+        #[cfg_attr(feature = "backtrace", backtrace)]
+        dynlink_nvidia_encode::NvencError,
+    ),
 }
 
 #[cfg(test)]
