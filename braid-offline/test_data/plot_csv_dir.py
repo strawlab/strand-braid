0937a7be8d3e21d0d203a99febfc140231b78dc6
Andrew Straw <strawman@astraw.com>
2021-07-26 16:06:11 +0200

    braid-offline: sample python script runs

--- braid-offline/test_data/plot_csv_dir.py
+++ braid-offline/test_data/plot_csv_dir.py
@@ -5,15 +5,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dirname = sys.argv[1]
-print dirname
+print(dirname)
 
 data2d_df = pd.read_csv(os.path.join(dirname,'data2d_distorted.csv'))
-kest_df = pd.read_csv(os.path.join(dirname,'kalman_estimates.csv'))
+traj3d_filename = os.path.join(dirname,'kalman_estimates.csv')
+if os.path.exists(traj3d_filename):
+    kest_df = pd.read_csv(traj3d_filename)
+else:
+    print('WARNING: no 3D data, unable to plot 3D')
+    kest_df = None
 
 fig = plt.figure()
 ax = fig.add_subplot(211)
-for obj_id, df in kest_df.groupby('obj_id'):
-    ax.plot( df['x']*1000.0, df['y']*1000.0, label='%d' % obj_id )
+if kest_df is not None:
+    for obj_id, df in kest_df.groupby('obj_id'):
+        ax.plot( df['x']*1000.0, df['y']*1000.0, label='%d' % obj_id )
 ax.legend()
 ax.set_xlabel('x (mm)')
 ax.set_ylabel('y (mm)')
@@ -27,17 +33,18 @@ ax.set_aspect('equal', adjustable='box')
 
 fig,axes = plt.subplots(nrows=4,ncols=1,sharex=True)
 axx, axy = axes[0], axes[1]
-for obj_id, df in kest_df.groupby('obj_id'):
-    axx.fill_between( df['frame'],
-        (df['x'] + np.sqrt(df['P00']))*1000.0,
-        (df['x'] - np.sqrt(df['P00']))*1000.0,
-        alpha = 0.2, edgecolor='none')
-    axx.plot( df['frame'], df['x']*1000.0 )
-    axy.fill_between( df['frame'],
-        (df['y'] + np.sqrt(df['P11']))*1000.0,
-        (df['y'] - np.sqrt(df['P11']))*1000.0,
-        alpha = 0.2, edgecolor='none')
-    axy.plot( df['frame'], df['y']*1000.0 )
+if kest_df is not None:
+    for obj_id, df in kest_df.groupby('obj_id'):
+        axx.fill_between( df['frame'],
+            (df['x'] + np.sqrt(df['P00']))*1000.0,
+            (df['x'] - np.sqrt(df['P00']))*1000.0,
+            alpha = 0.2, edgecolor='none')
+        axx.plot( df['frame'], df['x']*1000.0 )
+        axy.fill_between( df['frame'],
+            (df['y'] + np.sqrt(df['P11']))*1000.0,
+            (df['y'] - np.sqrt(df['P11']))*1000.0,
+            alpha = 0.2, edgecolor='none')
+        axy.plot( df['frame'], df['y']*1000.0 )
 
 axx.set_ylabel('x (mm)')
 axy.set_ylabel('y (mm)')
