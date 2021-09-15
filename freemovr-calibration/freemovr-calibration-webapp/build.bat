eec566f46153140faa0dbfd3957c7af6463ec485
Andrew Straw <strawman@astraw.com>
2021-09-15 08:07:56 +0200

    freemovr-calibration-webapp: script handle errors

--- freemovr-calibration/freemovr-calibration-webapp/build.bat
+++ freemovr-calibration/freemovr-calibration-webapp/build.bat
@@ -1,14 +1,19 @@
 REM Note, this is the wrong way to do things.
 REM See https://github.com/rustwasm/wasm-bindgen/pull/1994#issuecomment-608966482
-cargo build --target wasm32-unknown-unknown --release --bin main
-wasm-bindgen --target web --no-typescript --out-dir pkg --out-name main ../../target/wasm32-unknown-unknown/release/main.wasm
+cargo build --target wasm32-unknown-unknown --release --bin main || goto :error
+wasm-bindgen --target web --no-typescript --out-dir pkg --out-name main ../../target/wasm32-unknown-unknown/release/main.wasm || goto :error
 
-cargo build --target wasm32-unknown-unknown --release --bin native_worker
-wasm-bindgen --target no-modules --no-typescript --out-dir pkg --out-name native_worker ../../target/wasm32-unknown-unknown/release/native_worker.wasm
+cargo build --target wasm32-unknown-unknown --release --bin native_worker || goto :error
+wasm-bindgen --target no-modules --no-typescript --out-dir pkg --out-name native_worker ../../target/wasm32-unknown-unknown/release/native_worker.wasm || goto :error
 
-mkdir pkg
-copy static\index.html pkg
-copy static\style.css pkg
+copy static\index.html pkg || goto :error
+copy static\style.css pkg || goto :error
 
 REM Build OK. Now run with:
 REM     microserver --port 8000 --no-spa pkg
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
