862ad68786c58de3dc3113917c8c2ffb69d5ba0e
Andrew Straw <strawman@astraw.com>
2021-09-28 09:54:52 +0200

    braid-april-cal-webapp: error handling in script

--- braid-april-cal-webapp/build.bat
+++ braid-april-cal-webapp/build.bat
@@ -1,11 +1,17 @@
 REM Note, this is the wrong way to do things.
 REM See https://github.com/rustwasm/wasm-bindgen/pull/1994#issuecomment-608966482
-cargo build --target wasm32-unknown-unknown --release --bin main
-wasm-bindgen --target web --no-typescript --out-dir pkg --out-name main ../target/wasm32-unknown-unknown/release/main.wasm
+cargo build --target wasm32-unknown-unknown --release --bin main || goto :error
+wasm-bindgen --target web --no-typescript --out-dir pkg --out-name main ../target/wasm32-unknown-unknown/release/main.wasm || goto :error
 
-copy static\index.html pkg\
-copy static\style.css pkg\
+copy static\index.html pkg\ || goto :error
+copy static\style.css pkg\ || goto :error
 
 REM Build OK. Now run with:
 REM     microserver --port 8000 --no-spa pkg
 REM and visit http://localhost:8000/
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
