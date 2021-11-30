471d2e03c6da74dcfa9f43557ff3c6b3b70073f1
Andrew Straw <strawman@astraw.com>
2021-11-30 15:47:30 +0100

    select: cargo fmt

--- select/src/lib.rs
+++ select/src/lib.rs
@@ -6,7 +6,7 @@ pub enum Error {
     Timeout,
 }
 
-pub fn block_or_timeout(fd: RawFd, timeout_ms: u32) -> Result<(),Error> {
+pub fn block_or_timeout(fd: RawFd, timeout_ms: u32) -> Result<(), Error> {
     let mut timeout = libc::timeval {
         tv_sec: (timeout_ms / 1000) as libc::time_t,
         tv_usec: (timeout_ms % 1000 * 1000) as libc::suseconds_t,
@@ -15,9 +15,17 @@ pub fn block_or_timeout(fd: RawFd, timeout_ms: u32) -> Result<(),Error> {
     unsafe {
         let mut set: libc::fd_set = std::mem::zeroed();
         libc::FD_SET(fd, &mut set);
-        let rc = libc::select(fd + 1, &mut set as *mut _, 0 as *mut _, 0 as *mut _, &mut timeout);
+        let rc = libc::select(
+            fd + 1,
+            &mut set as *mut _,
+            0 as *mut _,
+            0 as *mut _,
+            &mut timeout,
+        );
         if rc < 0 {
-            Err(Error::Errno(std::io::Error::last_os_error().raw_os_error().unwrap()))
+            Err(Error::Errno(
+                std::io::Error::last_os_error().raw_os_error().unwrap(),
+            ))
         } else if rc == 0 {
             Err(Error::Timeout)
         } else {
