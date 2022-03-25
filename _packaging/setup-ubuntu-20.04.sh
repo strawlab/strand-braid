a50c0cf50e3e3276f48197818fd38de000f5917a
Andrew Straw <strawman@astraw.com>
2022-03-25 10:57:21 +0100

    CI: install pylon less often

--- _packaging/setup-ubuntu-20.04.sh
+++ _packaging/setup-ubuntu-20.04.sh
@@ -25,12 +25,6 @@ curl --show-error --fail --silent https://internal-static.strawlab.org/software/
 echo "b47f14efcb5cb35e7a17300094e2e5c7daba8bbdc6610a0463f5933cda61a1de /tmp/libvpx-opt-static_1.8.0-0ads1_amd64.deb" | sha256sum -c
 apt-get install /tmp/libvpx-opt-static_1.8.0-0ads1_amd64.deb
 
-# Download and install pylon 6
-# When updating this, also update the file ubuntu-2004-installer-zip-readme.txt and the Pylon version specified in strand-braid/debian/control
-curl --show-error --fail --silent https://internal-static.strawlab.org/software/pylon/pylon_6.2.0.21487-deb0_amd64.deb > /tmp/pylon_6.2.0.21487-deb0_amd64.deb
-echo "6acaf99a7331fde2b82217b15642a4e1ae96022bb13c7a91ed1a929ae664e391 /tmp/pylon_6.2.0.21487-deb0_amd64.deb" | sha256sum -c
-apt-get install /tmp/pylon_6.2.0.21487-deb0_amd64.deb
-
 curl --show-error --fail --silent https://internal-static.strawlab.org/software/opencv/opencv-3.2-static.tar.gz > /tmp/opencv-3.2-static.tar.gz
 echo "0316517e848ab3193b8d3ce2d7275602466dbd396e465b7aae5a9c7f342290d4  /tmp/opencv-3.2-static.tar.gz" | sha256sum -c
 tar xzf /tmp/opencv-3.2-static.tar.gz -C /
