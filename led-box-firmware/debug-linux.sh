df574a4adead2f0c03f0d48dfcf2ba19ac80b0b5
Andrew Straw <strawman@astraw.com>
2022-05-18 12:01:42 +0200

    led-box-firmware: remove unused scripts

--- led-box-firmware/debug-linux.sh
+++
@@ -1,1 +0,0 @@
-arm-none-eabi-gdb target_linux/thumbv7em-none-eabihf/release/led-box-firmware
--- led-box-firmware/make-linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-set -o errexit
-
-CARGO_TARGET_DIR=target_linux cargo build --release
-
-arm-none-eabi-objcopy -O binary target_linux/thumbv7em-none-eabihf/release/led-box-firmware target_linux/thumbv7em-none-eabihf/release/led-box-firmware.bin
-
