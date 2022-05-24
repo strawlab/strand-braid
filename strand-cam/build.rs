bdf5c4103ec1e04097d84bbd89b71f2c8d39ee76
Andrew Straw <strawman@astraw.com>
2022-05-25 01:29:42 +0200

    strand-cam: slightly better error message

--- strand-cam/build.rs
+++ strand-cam/build.rs
@@ -17,9 +17,17 @@ fn get_files_dir() -> std::path::PathBuf {
     ["yew_frontend", "pkg"].iter().collect()
 }
 
-fn main() {
+fn main() -> Result<(), Box<(dyn std::error::Error)>> {
     let git_rev = git_hash();
     println!("cargo:rustc-env=GIT_HASH={}", git_rev);
     let files_dir = get_files_dir();
-    bui_backend_codegen::codegen(&files_dir, "frontend.rs").expect("codegen failed");
+    let generated_path = "frontend.rs";
+    match bui_backend_codegen::codegen(&files_dir, &generated_path) {
+        Ok(()) => Ok(()),
+        Err(e) => Err(format!(
+            "Error in the process of generating '{generated_path}' when attempting to read {} : {e}",
+            files_dir.display()
+        )
+        .into()),
+    }
 }
