1599734ddbac2cb34011d6a62295b31f1913b5db
Andrew Straw <strawman@astraw.com>
2022-05-24 08:25:38 +0200

    strand-cam: 'cargo build' should just work

--- strand-cam/strand-cam-pylon/windows-build.bat
+++ strand-cam/strand-cam-pylon/windows-build.bat
@@ -22,6 +22,6 @@ set VPX_LIB_DIR=%HomeDrive%%HomePath%\libvpx_v1.10.0_msvc16\lib\x64
 set VPX_INCLUDE_DIR=%HomeDrive%%HomePath%\libvpx_v1.10.0_msvc16\include
 SET VPX_NO_PKG_CONFIG=1
 REM Could/should install april tag stuff and use `fiducial` feature. See https://github.com/jerry73204/apriltag-sys
-cargo build --features "strand-cam/bundle_files strand-cam/flydra_feat_detect strand-cam/imtrack-absdiff ipp-sys/2019 strand-cam/checkercal backtrace imops/simd" --release
+cargo build --no-default-features --features "strand-cam/bundle_files strand-cam/flydra_feat_detect strand-cam/imtrack-absdiff ipp-sys/2019 strand-cam/checkercal backtrace imops/simd" --release
 
 copy %HomeDrive%%HomePath%\Downloads\opencv\build\x64\vc14\bin\opencv_world455.dll ..\..\target\release\
