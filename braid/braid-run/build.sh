0cb09cdb65e7ce1a378c02bdd0e341d5551f15cb
Andrew Straw <strawman@astraw.com>
2022-05-26 11:11:13 +0200

    braid: remove outdated scripts

--- braid/braid-run/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-# Prerequisite: braid_frontend/pkg is built.
-
-RUSTFLAGS="$RUSTFLAGS -C target-cpu=sandybridge -C codegen-units=1" NUM_JOBS=2 cargo build --no-default-features --features "bundle_files backtrace" --release
