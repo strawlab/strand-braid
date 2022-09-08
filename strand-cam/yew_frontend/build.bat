6bc87b47f812ee245370c052335ea246b6f8a23a
Andrew Straw <strawman@astraw.com>
2022-09-08 17:50:35 +0200

    light-dark: fix paths on Win

--- strand-cam/yew_frontend/build.bat
+++ strand-cam/yew_frontend/build.bat
@@ -2,4 +2,4 @@ REM Install wasm-pack from here https://rustwasm.github.io/wasm-pack/installer/
 wasm-pack build --target web
 
 REM Install grass with: cargo install grass
-grass -I ../../../ads-webasm/scss scss/strand-cam-frontend.scss > pkg/style.css
+grass -I ..\..\ads-webasm\scss scss\strand-cam-frontend.scss pkg\style.css
