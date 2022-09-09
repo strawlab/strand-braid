c40aecf4f351ccb1da306ad0555127cd47dd59b6
Andrew Straw <strawman@astraw.com>
2022-09-09 08:28:59 +0200

    ui: fix win build scripts

--- ads-webasm-example/build.bat
+++ ads-webasm-example/build.bat
@@ -2,4 +2,4 @@ wasm-pack build --target web
 
 mkdir pkg
 copy static\index.html pkg
-grass -I ../ads-webasm/scss static/ads-webasm-example.scss > pkg/style.css
+grass -I ../ads-webasm/scss static/ads-webasm-example.scss pkg/style.css
--- braidz-viewer/build.bat
+++ braidz-viewer/build.bat
@@ -4,7 +4,7 @@ wasm-pack build --target web
 mkdir deploy
 copy pkg\* deploy
 copy static\* deploy
-grass -I ../ads-webasm/scss/ scss/braidz-viewer.scss > deploy/style.css
+grass -I ../ads-webasm/scss/ scss/braidz-viewer.scss deploy/style.css
 
 REM Build OK. Now run with:
 REM     microserver --port 8000 --no-spa deploy
