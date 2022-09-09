8bb419771426843dec7822151dfa5aafb2796eda
Andrew Straw <strawman@astraw.com>
2022-09-09 12:56:52 +0200

    ads-webasm: update to recent rust edition

--- ads-webasm/src/lib.rs
+++ ads-webasm/src/lib.rs
@@ -1,20 +1,3 @@
 #![recursion_limit = "1000"]
-
-extern crate wasm_bindgen;
-extern crate web_sys;
-
-extern crate chrono;
-extern crate http;
-extern crate serde;
-extern crate serde_yaml;
-extern crate uuid;
-extern crate yew;
-extern crate yew_tincture;
-
-extern crate bui_backend_types;
-extern crate enum_iter;
-extern crate http_video_streaming_types;
-extern crate rust_cam_bui_types;
-
 pub mod components;
 pub mod video_data;
