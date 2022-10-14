7164a6a79a9d8dd203ffd3fa48f5f2a6afb07c0b
Andrew Straw <strawman@astraw.com>
2022-10-14 14:39:22 +0200

    strand-cam: fix yew frontend build
    Recent wasm-pack will delete the contents of the `pkg` directory. So we
    should no longer expect otherwise.

--- strand-cam/yew_frontend/build.bat
+++ strand-cam/yew_frontend/build.bat
@@ -1,5 +1,10 @@
 REM Install wasm-pack from here https://rustwasm.github.io/wasm-pack/installer/
+
+REM This will build the source and place results into a new `pkg` dir
 wasm-pack build --target web
 
 REM Install grass with: cargo install grass
 grass -I ..\..\ads-webasm\scss scss\strand-cam-frontend.scss pkg\style.css
+
+copy static\index.html pkg\index.html
+copy static\strand-camera-no-text.png pkg\strand-camera-no-text.png
--- strand-cam/yew_frontend/build.sh
+++ strand-cam/yew_frontend/build.sh
@@ -2,10 +2,15 @@
 set -o errexit
 
 # Install wasm-pack from here https://rustwasm.github.io/wasm-pack/installer/
+
+# This will build the source and place results into a new `pkg` dir
 wasm-pack build --target web
 
 # Install grass with: cargo install grass
 grass -I ../../ads-webasm/scss scss/strand-cam-frontend.scss pkg/style.css
+
+cp static/index.html pkg/index.html
+cp static/strand-camera-no-text.png pkg/strand-camera-no-text.png
 
 # above built everything, let's now run it locally
 # (install with `cargo install microserver`)
