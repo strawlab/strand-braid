bc149161d52a15bfeb62b0c59352b12dd7690cbb
Andrew Straw <strawman@astraw.com>
2022-10-14 15:42:46 +0200

    braid: fix yew frontend build

--- braid/braid-run/braid_frontend/build.bat
+++ braid/braid-run/braid_frontend/build.bat
@@ -1,5 +1,10 @@
 REM Install wasm-pack from here https://rustwasm.github.io/wasm-pack/installer/
+
+REM This will build the source and place results into a new `pkg` dir
 wasm-pack build --target web
 
 REM Install grass with: cargo install grass
 grass -I ../../../ads-webasm/scss scss/braid_frontend.scss pkg/style.css
+
+copy static\braid-logo-no-text.png pkg\braid-logo-no-text.png
+copy static\index.html pkg\index.html
--- braid/braid-run/braid_frontend/build.sh
+++ braid/braid-run/braid_frontend/build.sh
@@ -2,10 +2,15 @@
 set -o errexit
 
 # Install wasm-pack from here https://rustwasm.github.io/wasm-pack/installer/
+
+# This will build the source and place results into a new `pkg` dir
 wasm-pack build --target web
 
 # Install grass with: cargo install grass
 grass -I ../../../ads-webasm/scss scss/braid_frontend.scss pkg/style.css
+
+cp static/braid-logo-no-text.png pkg/braid-logo-no-text.png
+cp static/index.html pkg/index.html
 
 # above built everything, let's now run it locally
 # (install with `cargo install microserver`)
