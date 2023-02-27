91960969202d01e15c29132bf94abcf290fd5aaf
Andrew Straw <strawman@astraw.com>
2023-02-27 20:55:30 +0100

    zip-or-dir: add license and other metadata

--- zip-or-dir/dir2zip/src/main.rs
+++ zip-or-dir/dir2zip/src/main.rs
@@ -1,3 +1,10 @@
+// Copyright 2020-2023 Andrew D. Straw.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT
+// or http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 fn main() -> anyhow::Result<()> {
     let args: Vec<String> = std::env::args().map(String::from).collect();
     if args.len() != 3 {
