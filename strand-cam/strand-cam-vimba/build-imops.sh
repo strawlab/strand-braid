881d3306d368ad85ea6c790d84c0dd4872f0726a
Andrew Straw <strawman@astraw.com>
2023-03-03 15:47:20 +0100

    strand-cam: update build script

--- strand-cam/strand-cam-vimba/build-imops.sh
+++ strand-cam/strand-cam-vimba/build-imops.sh
@@ -6,4 +6,4 @@ set -o errexit
 export VIMBAC_LIBDIR="/opt/Vimba_6_0/VimbaC/DynamicLib/x86_64bit"
 export PKG_CONFIG_PATH=/opt/opencv-4.5.5-static/lib/pkgconfig:/opt/libvpx/libvpx-1.8.0/lib/pkgconfig
 
-cargo +nightly build --no-default-features --features bundle_files,backend_vimba,backtrace,ci2-vimba/backtrace --release
+cargo +nightly build --features backtrace,ci2-vimba/backtrace,imops/simd --release
