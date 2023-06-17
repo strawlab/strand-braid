0c85ac8b0c72749b8166431b98f933b89973f025
Andrew Straw <strawman@astraw.com>
2023-06-17 06:33:22 +0200

    braid-april-cal-webapp: upload .htaccess file

--- braid-april-cal-webapp/upload.sh
+++ braid-april-cal-webapp/upload.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -x
 set -o errexit
 
+# Create pkg/.htaccess file
+mkdir -p pkg
+cat <<EOF > pkg/.htaccess
+AddType application/wasm                            wasm
+EOF
+
+# Upload pkg directory
 rsync -avzP --delete pkg/ strawlab-org:strawlab.org/braid-april-cal-webapp/
