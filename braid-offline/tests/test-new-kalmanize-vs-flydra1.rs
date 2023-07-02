f86588098019c4920cb9332c57eab4475d0753e5
Andrew Straw <strawman@astraw.com>
2023-07-02 08:27:35 +0200

    braid-offline: better test names

--- braid-offline/tests/test-new-kalmanize-vs-flydra1.rs
+++ braid-offline/tests/test-new-kalmanize-vs-flydra1.rs
@@ -379,7 +379,7 @@ async fn run_test(src: &str, untracked_dir: PathBuf) -> anyhow::Result<()> {
 }
 
 #[tokio::test]
-async fn do_test() -> anyhow::Result<()> {
+async fn test_braid_vs_old_flydra() -> anyhow::Result<()> {
     let _ = env_logger::builder().is_test(true).try_init();
 
     let src = "../_submodules/flydra/flydra_analysis/flydra_analysis/a2/sample_datafile-v0.4.28.h5";
@@ -396,7 +396,7 @@ async fn do_test() -> anyhow::Result<()> {
 }
 
 #[tokio::test]
-async fn do_water_test() -> anyhow::Result<()> {
+async fn test_braid_vs_old_flydra_with_water() -> anyhow::Result<()> {
     const FNAME: &str = "20160527_163937.mainbrain-short.h5";
     const URL_BASE: &str = "https://strawlab-cdn.com/assets";
     const SHA256SUM: &str = "7a63749cea63853ad1b9b2f6c32c087459a7be52aaef8730b0a41f00c5807d1b";
