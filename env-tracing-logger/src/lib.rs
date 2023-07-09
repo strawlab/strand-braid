055ac2a9c9fdc6bff05c811cea20e9e3e4bc00a5
Andrew Straw <strawman@astraw.com>
2023-07-09 17:24:29 +0200

    env-tracing-logger: use only tracing

--- env-tracing-logger/src/lib.rs
+++ env-tracing-logger/src/lib.rs
@@ -1,15 +1,28 @@
 use tracing_subscriber::prelude::*;
 
+struct Guard {}
+
+impl Drop for Guard {
+    fn drop(&mut self) {}
+}
+
 pub fn init() -> impl Drop {
-    let fmt_layer = tracing_subscriber::fmt::Layer::default();
+    // let fmt_layer = tracing_subscriber::fmt::Layer::default();
 
-    let (flame_layer, _guard) = tracing_flame::FlameLayer::with_file("./tracing.folded").unwrap();
+    // let (flame_layer, _guard) = tracing_flame::FlameLayer::with_file("./tracing.folded").unwrap();
 
-    let subscriber = tracing_subscriber::registry::Registry::default()
-        .with(fmt_layer)
-        .with(flame_layer);
+    // let subscriber = tracing_subscriber::registry::Registry::default()
+    //     .with(fmt_layer)
+    //     .with(flame_layer);
 
-    tracing_log::LogTracer::init().unwrap();
+    let subscriber = tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::EnvFilter::from_default_env());
+    // .init();
+
+    // tracing_log::LogTracer::init().unwrap();
+
+    let _guard = Guard {};
 
     tracing::subscriber::set_global_default(subscriber).expect("Could not set global default");
     _guard
