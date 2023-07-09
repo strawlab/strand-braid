de9097303acbd99b6eabd6365ee2656c81aefe76
Andrew Straw <strawman@astraw.com>
2023-07-09 17:41:33 +0200

    env-tracking-logger: save tracing data for flame chart

--- env-tracing-logger/src/lib.rs
+++ env-tracing-logger/src/lib.rs
@@ -1,15 +1,15 @@
 use tracing_subscriber::prelude::*;
 
-struct Guard {}
+// struct Guard {}
 
-impl Drop for Guard {
-    fn drop(&mut self) {}
-}
+// impl Drop for Guard {
+//     fn drop(&mut self) {}
+// }
 
 pub fn init() -> impl Drop {
     // let fmt_layer = tracing_subscriber::fmt::Layer::default();
 
-    // let (flame_layer, _guard) = tracing_flame::FlameLayer::with_file("./tracing.folded").unwrap();
+    let (flame_layer, _guard) = tracing_flame::FlameLayer::with_file("./tracing.folded").unwrap();
 
     // let subscriber = tracing_subscriber::registry::Registry::default()
     //     .with(fmt_layer)
@@ -17,12 +17,13 @@ pub fn init() -> impl Drop {
 
     let subscriber = tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
-        .with(tracing_subscriber::EnvFilter::from_default_env());
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .with(flame_layer);
     // .init();
 
     // tracing_log::LogTracer::init().unwrap();
 
-    let _guard = Guard {};
+    // let _guard = Guard {};
 
     tracing::subscriber::set_global_default(subscriber).expect("Could not set global default");
     _guard
