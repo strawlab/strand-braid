0bb5c57f8cd14320a1d97dfb53bd50e960395de2
Andrew Straw <strawman@astraw.com>
2023-07-15 14:47:48 +0200

    braid-offline: test retracking

--- braid-offline/src/bin/braid-offline-retrack.rs
+++ braid-offline/src/bin/braid-offline-retrack.rs
@@ -1,35 +1,5 @@
-use anyhow::Context;
-
 use clap::Parser;
-use tracing::info;
 use tracing_futures::Instrument;
-
-#[derive(Parser, Debug)]
-#[command(author, version, about)]
-struct Cli {
-    /// Input .braidz file
-    #[arg(short = 'd', long)]
-    data_src: std::path::PathBuf,
-    /// Output file (must end with .braidz)
-    #[arg(short = 'o', long)]
-    output: std::path::PathBuf,
-    /// Set frames per second
-    #[arg(long)]
-    fps: Option<f64>,
-    /// Set start frame to start tracking
-    #[arg(long)]
-    start_frame: Option<u64>,
-    /// Set stop frame to stop tracking
-    #[arg(long)]
-    stop_frame: Option<u64>,
-    /// Tracking parameters TOML file.
-    #[arg(long)]
-    tracking_params: Option<std::path::PathBuf>,
-
-    /// Disable display of progress indicator
-    #[arg(long)]
-    no_progress: bool,
-}
 
 fn main() -> anyhow::Result<()> {
     if std::env::var_os("RUST_LOG").is_none() {
@@ -38,7 +8,10 @@ fn main() -> anyhow::Result<()> {
 
     // console_subscriber::init();
     let _tracing_guard = env_tracing_logger::init();
-    let future = async { braid_offline_retrack().await };
+
+    let opt = braid_offline::Cli::parse();
+
+    let future = async { braid_offline::braid_offline_retrack(opt).await };
     let instrumented = future.instrument(tracing::info_span!("braid-offline-retrack"));
 
     // Multi-threaded runtime
@@ -58,86 +31,4 @@ fn main() -> anyhow::Result<()> {
 
     // spawn the root task
     rt.block_on(instrumented)
-}
-
-/// This is our "real" main top-level function but we have some decoration we
-/// need to do in [main], so we name this differently.
-#[tracing::instrument]
-async fn braid_offline_retrack() -> anyhow::Result<()> {
-    let opt = Cli::parse();
-
-    let data_src =
-        braidz_parser::incremental_parser::IncrementalParser::open(opt.data_src.as_path())?;
-    let data_src = data_src.parse_basics().with_context(|| {
-        format!(
-            "when parsing braidz file {}",
-            opt.data_src.as_path().display()
-        )
-    })?;
-
-    let tracking_params: flydra_types::TrackingParams = match opt.tracking_params {
-        Some(ref fname) => {
-            info!("reading tracking parameters from file {}", fname.display());
-            // read the traking parameters
-            let buf = std::fs::read_to_string(fname)
-                .context(format!("loading tracking parameters {}", fname.display()))?;
-            let tracking_params: flydra_types::TrackingParams = toml::from_str(&buf)?;
-            tracking_params
-        }
-        None => {
-            let parsed = data_src.basic_info();
-            match parsed.tracking_params.clone() {
-                Some(tp) => tp,
-                None => {
-                    let num_cams = data_src.basic_info().cam_info.camid2camn.len();
-                    match num_cams {
-                        0 => {
-                            anyhow::bail!(
-                                "No tracking parameters specified, none found in \
-                            data_src, and no default is reasonable because zero cameras present."
-                            )
-                        }
-                        1 => flydra_types::default_tracking_params_flat_3d(),
-                        _ => flydra_types::default_tracking_params_full_3d(),
-                    }
-                }
-            }
-        }
-    };
-    let opts = braid_offline::KalmanizeOptions {
-        start_frame: opt.start_frame,
-        stop_frame: opt.stop_frame,
-        ..Default::default()
-    };
-
-    // The user specifies an output .braidz file. But we will save initially to
-    // a .braid directory. We here ensure the user's name had ".braidz"
-    // extension and then calculate the name of the new output directory.
-    let output_braidz = opt.output;
-
-    // Raise an error if outputs exist.
-    if output_braidz.exists() {
-        return Err(anyhow::format_err!(
-            "Path {} exists. Will not overwrite.",
-            output_braidz.display()
-        ));
-    }
-
-    let rt_handle = tokio::runtime::Handle::current();
-
-    let save_performance_histograms = true;
-
-    braid_offline::kalmanize(
-        data_src,
-        output_braidz,
-        opt.fps,
-        tracking_params,
-        opts,
-        rt_handle,
-        save_performance_histograms,
-        "braid-offline-retrack",
-        opt.no_progress,
-    )
-    .await?;
-    Ok(())
 }
--- braid-offline/src/lib.rs
+++ braid-offline/src/lib.rs
@@ -10,6 +10,8 @@ use std::{
     path::{Path, PathBuf},
 };
 
+use anyhow::Context;
+use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
 use tracing::{debug, info, warn};
 
@@ -699,4 +701,117 @@ pub fn pick_csvgz_or_csv(csv_path: &Path) -> flydra2::Result<Box<dyn Read>> {
         let decoder = libflate::gzip::Decoder::new(gz_fd)?;
         Ok(Box::new(decoder))
     }
+}
+
+/// This is our "real" main top-level function but we have some decoration we
+/// need to do in [main], so we name this differently.
+#[tracing::instrument]
+pub async fn braid_offline_retrack(opt: Cli) -> anyhow::Result<()> {
+    let data_src =
+        braidz_parser::incremental_parser::IncrementalParser::open(opt.data_src.as_path())
+            .with_context(|| {
+                format!(
+                    "while opening file \"{}\"",
+                    opt.data_src.as_path().display()
+                )
+            })?;
+    let data_src = data_src.parse_basics().with_context(|| {
+        format!(
+            "when parsing braidz file \"{}\"",
+            opt.data_src.as_path().display()
+        )
+    })?;
+
+    let tracking_params: flydra_types::TrackingParams = match opt.tracking_params {
+        Some(ref fname) => {
+            info!("reading tracking parameters from file {}", fname.display());
+            // read the traking parameters
+            let buf = std::fs::read_to_string(fname)
+                .context(format!("loading tracking parameters {}", fname.display()))?;
+            let tracking_params: flydra_types::TrackingParams = toml::from_str(&buf)?;
+            tracking_params
+        }
+        None => {
+            let parsed = data_src.basic_info();
+            match parsed.tracking_params.clone() {
+                Some(tp) => tp,
+                None => {
+                    let num_cams = data_src.basic_info().cam_info.camid2camn.len();
+                    match num_cams {
+                        0 => {
+                            anyhow::bail!(
+                                "No tracking parameters specified, none found in \
+                            data_src, and no default is reasonable because zero cameras present."
+                            )
+                        }
+                        1 => flydra_types::default_tracking_params_flat_3d(),
+                        _ => flydra_types::default_tracking_params_full_3d(),
+                    }
+                }
+            }
+        }
+    };
+    let opts = KalmanizeOptions {
+        start_frame: opt.start_frame,
+        stop_frame: opt.stop_frame,
+        ..Default::default()
+    };
+
+    // The user specifies an output .braidz file. But we will save initially to
+    // a .braid directory. We here ensure the user's name had ".braidz"
+    // extension and then calculate the name of the new output directory.
+    let output_braidz = opt.output;
+
+    // Raise an error if outputs exist.
+    if output_braidz.exists() {
+        return Err(anyhow::format_err!(
+            "Path {} exists. Will not overwrite.",
+            output_braidz.display()
+        ));
+    }
+
+    let rt_handle = tokio::runtime::Handle::current();
+
+    let save_performance_histograms = true;
+
+    kalmanize(
+        data_src,
+        output_braidz,
+        opt.fps,
+        tracking_params,
+        opts,
+        rt_handle,
+        save_performance_histograms,
+        "braid-offline-retrack",
+        opt.no_progress,
+    )
+    .await?;
+    Ok(())
+}
+
+#[derive(Parser, Debug, Default)]
+#[command(author, version, about)]
+pub struct Cli {
+    /// Input .braidz file
+    #[arg(short = 'd', long)]
+    pub data_src: std::path::PathBuf,
+    /// Output file (must end with .braidz)
+    #[arg(short = 'o', long)]
+    pub output: std::path::PathBuf,
+    /// Set frames per second
+    #[arg(long)]
+    pub fps: Option<f64>,
+    /// Set start frame to start tracking
+    #[arg(long)]
+    pub start_frame: Option<u64>,
+    /// Set stop frame to stop tracking
+    #[arg(long)]
+    pub stop_frame: Option<u64>,
+    /// Tracking parameters TOML file.
+    #[arg(long)]
+    pub tracking_params: Option<std::path::PathBuf>,
+
+    /// Disable display of progress indicator
+    #[arg(long)]
+    pub no_progress: bool,
 }
