9fe5ffb82c396ddaa1217a3d0ee1f8119800b420
Andrew Straw <strawman@astraw.com>
2023-08-30 16:08:27 +0200

    simple-obj-parse: clippy fixes

--- simple-obj-parse/tests/integration.rs
+++ simple-obj-parse/tests/integration.rs
@@ -6,10 +6,7 @@ fn test_read_32vert_polygon() {
 
     // This might fail but it should not panic. See
     // https://github.com/kvark/obj/pull/10
-    match simple_obj_parse::obj_parse(buf) {
-        Ok(_) => {}
-        Err(_) => {}
-    };
+    if let Ok(_) = simple_obj_parse::obj_parse(buf) {}
 }
 
 #[test]
