ff83b86ba5877d58cc4ec2fbccba1f3e55677100
Andrew Straw <strawman@astraw.com>
2023-08-30 10:23:20 +0200

    braid-run: 'cargo fmt'

--- braid/braid-run/src/main.rs
+++ braid/braid-run/src/main.rs
@@ -1,7 +1,4 @@
-#![cfg_attr(
-    feature = "backtrace",
-    feature(error_generic_member_access)
-)]
+#![cfg_attr(feature = "backtrace", feature(error_generic_member_access))]
 
 use clap::Parser;
 
