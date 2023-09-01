d23074046cfa963d0220e9bbe1390d08a0cd2a14
Andrew Straw <strawman@astraw.com>
2023-09-01 10:31:09 +0200

    braid-offline: fixup script

--- braid-offline/test_data/retrack-demo.sh
+++ braid-offline/test_data/retrack-demo.sh
@@ -1,20 +1,25 @@
-#!/bin/bash
+#!/bin/bash -x
 set -o errexit
 
 BASE=../_submodules/flydra/flydra_analysis/flydra_analysis/a2/sample_datafile-v0.4.28
-RETRACKED=/tmp/kalmanized
+RETRACKED=`mktemp -d -t retrackedXXXXXXX`
 
 NGCU=../strand-braid-user
 
 # Convert original flydra file to .csv
 python $NGCU/scripts/export_h5_to_csv.py "$BASE.h5"
 # Retrack .csv files
-cargo build
-RUST_LOG=flydra2=trace ../target/debug/braid-offline-retrack -d "$BASE" -o "$RETRACKED"
+cargo build --release
+BUILD_DIR=../target/release
+$BUILD_DIR/braid-offline-retrack -d "$BASE" -o "$RETRACKED.braidz"
 
 # Convert to pytables .h5 format
 rm -f "$RETRACKED.braidz.h5"
-PATH="../target/debug:$PATH" python $NGCU/scripts/convert_braidz_to_flydra_h5.py "$RETRACKED.braidz"
+PATH="$BUILD_DIR:$PATH" python $NGCU/scripts/convert_braidz_to_flydra_h5.py "$RETRACKED.braidz"
 
 # flydra_analysis_plot_timeseries_2d_3d "$BASE".h5 -k "$BASE".h5 --disable-kalman-smoothing
 flydra_analysis_plot_timeseries_2d_3d "$RETRACKED".braidz.h5 -k "$RETRACKED".braidz.h5 --disable-kalman-smoothing
+
+rm "$RETRACKED.braidz.h5"
+rm "$RETRACKED.unconverted.zip"
+rm -rf "$RETRACKED"
