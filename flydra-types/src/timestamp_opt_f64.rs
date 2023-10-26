b6490c952cd6c48867cc39bf8f621f183aac8fe3
Andrew Straw <strawman@astraw.com>
2023-10-26 08:19:53 +0200

    move several crates to MIT/Apache license

--- flydra-types/src/timestamp_opt_f64.rs
+++ flydra-types/src/timestamp_opt_f64.rs
@@ -1,3 +1,10 @@
+// Copyright 2020-2023 Andrew D. Straw.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT
+// or http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 /// serde helpers for `Option<FlydraFloatTimestampLocal>` to store as f64.
 ///
 /// A `None` value will be represented as a floating point NaN.
