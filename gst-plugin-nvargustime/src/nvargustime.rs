756c12a61c5c5f859d9988ba73341466c6b806fb
Andrew Straw <strawman@astraw.com>
2024-03-28 07:32:28 +0100

    gst-plugin-nvargustime: fix compilation warnings

--- gst-plugin-nvargustime/src/nvargustime.rs
+++ gst-plugin-nvargustime/src/nvargustime.rs
@@ -7,11 +7,9 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use glib;
 use glib::prelude::*;
 use glib::subclass;
 use glib::subclass::prelude::*;
-use gst;
 use gst::prelude::*;
 use gst::subclass::prelude::*;
 
