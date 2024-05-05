656f00a0342a6feca0757b6840acc1b056409f1d
Andrew Straw <strawman@astraw.com>
2024-05-05 09:35:35 +0200

    ci: use opencv 4.9.0

--- _packaging/setup-opencv-4.9.0-static.sh
+++ _packaging/setup-opencv-4.9.0-static.sh
@@ -8,6 +8,6 @@ set -o errexit
 # setup time of the build environment to a minimum and thus we use our own
 # packaging of opencv. This is substantially faster to install.
 
-curl --show-error --fail --silent https://internal-static.strawlab.org/software/opencv/opencv-4.5.5-static.tar.gz > /tmp/opencv-4.5.5-static.tar.gz
-echo "6dfc8bed523fd1833beb2bdde264863dc4cf49670e635bc987f01fd85638a7e6  /tmp/opencv-4.5.5-static.tar.gz" | sha256sum -c
-tar xzf /tmp/opencv-4.5.5-static.tar.gz -C /
+curl --show-error --fail --silent https://internal-static.strawlab.org/software/opencv/opencv-4.9.0-static.tar.gz > /tmp/opencv-4.9.0-static.tar.gz
+echo "612c071481f37d54552bc8e1d64dc6f89ece1d3140513c582639fe08c1a29cea  /tmp/opencv-4.9.0-static.tar.gz" | sha256sum -c
+tar xzf /tmp/opencv-4.9.0-static.tar.gz -C /
