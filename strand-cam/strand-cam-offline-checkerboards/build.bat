75b446fe5e7e0d2b6d0309364cf40bc17520370a
Andrew Straw <strawman@astraw.com>
2024-07-18 18:00:16 +0200

    strand-cam-offline-checkerboards: add build script for Windows

---
+++ strand-cam/strand-cam-offline-checkerboards/build.bat
@@ -0,0 +1,12 @@
+REM Download opencv-4.5.5-vc14_vc15.exe from https://github.com/opencv/opencv/releases/download/4.5.5/opencv-4.5.5-vc14_vc15.exe
+REM then expand it in your Downloads directory.
+
+set OPENCV_VERSION=455
+set OPENCV_LIB_DIR=%HomeDrive%%HomePath%\Downloads\opencv\build\x64\vc15\lib
+set OPENCV_INCLUDE_DIR=%HomeDrive%%HomePath%\Downloads\opencv\build\include
+
+REM Now build the binary
+
+cargo build --release
+
+copy %HomeDrive%%HomePath%\Downloads\opencv\build\x64\vc14\bin\opencv_world455.dll ..\..\target\release\
