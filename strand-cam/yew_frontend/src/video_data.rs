db965eada0472ee9d25474556eed552fd62f86d8
Andrew Straw <strawman@astraw.com>
2024-08-12 12:30:19 +0200

    strand-cam: fix compiler warnings

--- strand-cam/yew_frontend/src/video_data.rs
+++ strand-cam/yew_frontend/src/video_data.rs
@@ -4,20 +4,10 @@ pub struct VideoData {
 }
 
 impl VideoData {
-    pub fn new(inner: Option<http_video_streaming_types::ToClient>) -> Self {
+    pub(crate) fn new(inner: Option<http_video_streaming_types::ToClient>) -> Self {
         Self { inner }
     }
-
-    pub fn frame_number(&self) -> Option<u64> {
-        let result = self.inner.as_ref().map(|x| x.fno);
-        result
-    }
-
-    pub fn as_ref(&self) -> Option<&http_video_streaming_types::ToClient> {
-        self.inner.as_ref()
-    }
-
-    pub fn take(&mut self) -> Option<http_video_streaming_types::ToClient> {
+    pub(crate) fn take(&mut self) -> Option<http_video_streaming_types::ToClient> {
         self.inner.take()
     }
 }
