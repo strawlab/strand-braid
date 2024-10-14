b9fad9b07f67d06b51f16fc27d4f7964fb2edbed
Andrew Straw <strawman@astraw.com>
2024-10-14 11:34:01 +0200

    remove more old Vimba 6 stuff

--- strand-cam/strand-cam-vimba/build-imops.sh
+++ strand-cam/strand-cam-vimba/build-imops.sh
@@ -3,7 +3,6 @@ set -o errexit
 
 # Prerequisite: ../yew_frontend/pkg is built. Do this by "build-imops.sh" in yew_frontend.
 
-export VIMBAC_LIBDIR="/opt/Vimba_6_0/VimbaC/DynamicLib/x86_64bit"
 export PKG_CONFIG_PATH=/opt/opencv-4.5.5-static/lib/pkgconfig
 
 cargo +nightly build --features backtrace,ci2-vimba/backtrace,imops/simd --release
--- strand-cam/strand-cam-vimba/build-plain.sh
+++ strand-cam/strand-cam-vimba/build-plain.sh
@@ -3,9 +3,7 @@ set -o errexit
 
 # Prerequisite: ../yew_frontend/pkg is built. Do this by "build.sh" in yew_frontend.
 
-export VIMBAC_LIBDIR="/opt/Vimba_6_0/VimbaC/DynamicLib/x86_64bit"
-
 PKG_CONFIG_PATH=/opt/libvpx/libvpx-1.8.0/lib/pkgconfig \
-RUSTFLAGS="$RUSTFLAGS -C target-cpu=sandybridge -C codegen-units=1 -C link-args=-Wl,-rpath,/opt/Vimba_6_0/VimbaC/DynamicLib/x86_64bit" \
+RUSTFLAGS="$RUSTFLAGS -C target-cpu=sandybridge -C codegen-units=1" \
 NUM_JOBS=2 \
 cargo build --features "strand-cam/imtrack-absdiff strand-cam/bundle_files backtrace" --release
