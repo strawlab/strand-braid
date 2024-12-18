32f730d93b0877d9cb8fdd44bee88807a81aeeaa
Andrew Straw <strawman@astraw.com>
2024-12-18 14:42:05 +0100

    ffmpeg-rewriter: new crate

---
+++ media-utils/ffmpeg-rewriter/examples/save-animation.rs
@@ -0,0 +1,125 @@
+// Copyright 2022-2024 Andrew D. Straw.
+use font_drawing::stamp_frame;
+use machine_vision_formats::{
+    image_ref::ImageRef,
+    owned::OImage,
+    pixel_format::{Mono8, RGB8},
+    ImageMutStride, Stride,
+};
+
+use rusttype::Font;
+
+fn main() -> eyre::Result<()> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    env_tracing_logger::init();
+
+    // 1970-01-01 01:00:00 UTC
+    let start = chrono::DateTime::from_timestamp(60 * 60, 0).unwrap();
+
+    let image = image::load_from_memory(&include_bytes!("bee.jpg")[..])?;
+    let rgb = convert_image::image_to_rgb8(image)?;
+    let rgb = OImage::from_owned(rgb);
+
+    for width_pad in [0, 2] {
+        for height_pad in [0, 2] {
+            for format_str in &["mono8", "rgb8"] {
+                use machine_vision_formats::ImageData;
+
+                let final_width = rgb.width() - width_pad;
+                let final_height = rgb.height() - height_pad;
+                // let output_fname = format!("bee-{final_width}x{final_height}-{format_str}.y4m");
+                let output_fname = format!("bee-{final_width}x{final_height}-{format_str}.mp4");
+
+                let ffmpeg_codec_args = ffmpeg_writer::platform_hardware_encoder()?;
+
+                let rate = None;
+                let h264_metadata = None;
+
+                let dt_msec = 5;
+                let sample_duration = chrono::Duration::try_milliseconds(dt_msec).unwrap();
+
+                let mut my_ffmpeg_writer = ffmpeg_rewriter::FfmpegReWriter::new(
+                    &output_fname,
+                    ffmpeg_codec_args,
+                    rate,
+                    h264_metadata,
+                )?;
+
+                // Load the font
+                // let font_data = include_bytes!("../Roboto-Regular.ttf");
+                let font_data = ttf_firacode::REGULAR;
+                // This only succeeds if collection consists of one font
+                let font =
+                    Font::try_from_bytes(font_data as &[u8]).expect("Error constructing Font");
+
+                let mut count = 0;
+                let mut istart = std::time::Instant::now();
+
+                loop {
+                    let ts = start.checked_add_signed(sample_duration * count).unwrap();
+
+                    if count % 100 == 0 {
+                        let el = istart.elapsed();
+                        let elf = el.as_secs() as f64 + 1e-9 * el.subsec_nanos() as f64;
+                        println!(
+                            "frame {}, duration {} msec, timestamp {}",
+                            count,
+                            elf * 1000.0,
+                            ts
+                        );
+                        istart = std::time::Instant::now();
+                    }
+                    if count > 1000 {
+                        break;
+                    }
+
+                    // The text to render
+                    let text = format!("{}", ts);
+                    let mut frame = rgb.clone();
+
+                    stamp_frame(&mut frame as &mut dyn ImageMutStride<_>, &font, &text)?;
+                    count += 1;
+
+                    match *format_str {
+                        "mono8" => {
+                            // convert to farget format, keeping full size
+                            let mono = convert_image::convert_ref::<_, Mono8>(&frame)?;
+
+                            let out_size_bytes = mono.stride() * final_height as usize;
+                            let trimmed = OImage::<Mono8>::new(
+                                final_width,
+                                final_height,
+                                mono.stride().try_into().unwrap(),
+                                mono.image_data()[..out_size_bytes].to_vec(),
+                            )
+                            .unwrap();
+
+                            my_ffmpeg_writer.write_frame(&trimmed, ts)?;
+                        }
+                        "rgb8" => {
+                            let out_size_bytes = frame.stride() * final_height as usize;
+                            let trimmed = ImageRef::<RGB8>::new(
+                                final_width,
+                                final_height,
+                                frame.stride(),
+                                &frame.image_data()[..out_size_bytes],
+                            )
+                            .unwrap();
+
+                            my_ffmpeg_writer.write_frame(&trimmed, ts)?;
+                        }
+                        _ => {
+                            panic!("unknown format");
+                        }
+                    }
+                }
+
+                my_ffmpeg_writer.close()?;
+            }
+        }
+    }
+
+    Ok(())
+}
---
+++ media-utils/ffmpeg-rewriter/src/lib.rs
@@ -0,0 +1,303 @@
+use chrono::{DateTime, Local};
+use frame_source::{h264_source::SeekableH264Source, FrameDataSource};
+use machine_vision_formats as formats;
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+use ci2_remote_control::{H264Metadata, Mp4Codec, Mp4RecordingConfig, RecordingFrameRate};
+use ffmpeg_writer::{FfmpegCodecArgs, FfmpegWriter};
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("ffmpeg_writer {0}")]
+    FfmpegWriter(#[from] ffmpeg_writer::Error),
+    #[error("cannot reencode")]
+    CannotReencode,
+    #[error("filename does not end with '.mp4'")]
+    FilenameDoesNotEndWithMp4,
+    #[error("filename not unicode")]
+    FilenameNotUnicode,
+    #[error("source does not contain H264 video")]
+    SourceIsNotH264,
+    #[error("MP4 writer error: {0}")]
+    Mp4WriterError(#[from] mp4_writer::Error),
+    #[error("frame source error: {0}")]
+    FrameSourceError(#[from] frame_source::Error),
+    #[error("serde json error: {0}")]
+    SerdeJsonError(#[from] serde_json::Error),
+}
+type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Serialize, Deserialize)]
+struct SrtMsg {
+    timestamp: DateTime<chrono::Local>,
+}
+
+/// Save to a video using [FfmpegWriter] but when done, read the newly-written
+/// file and resave the data (without transcoding) with timestamps and other
+/// metadata.
+pub struct FfmpegReWriter {
+    mp4_filename: String,
+    ffmpeg_wtr: FfmpegWriter,
+    mp4_cfg: Mp4RecordingConfig,
+    srt_file_path: String,
+    swtr: srt_writer::BufferingSrtFrameWriter,
+    json_file_path: Option<String>,
+}
+
+impl FfmpegReWriter {
+    pub fn new(
+        mp4_path: impl AsRef<std::path::Path>,
+        ffmpeg_codec_args: FfmpegCodecArgs,
+        rate: Option<(usize, usize)>,
+        h264_metadata: Option<H264Metadata>,
+    ) -> Result<Self> {
+        let mp4_filename = PathBuf::from(mp4_path.as_ref())
+            .into_os_string()
+            .into_string()
+            .map_err(|_| Error::FilenameNotUnicode)?;
+        let basename = if let Some(basename) = mp4_filename.strip_suffix(".mp4") {
+            basename
+        } else {
+            return Err(Error::FilenameDoesNotEndWithMp4);
+        };
+
+        let srt_file_path = format!("{basename}.srt");
+        let json_file_path = if let Some(h264_metadata) = &h264_metadata {
+            // Save the metadata to a file in case we crash before
+            // Self::close(). That way this information can be recovered.
+            let jpath = format!("{basename}-metadata.json");
+            let buf = serde_json::to_string(h264_metadata)?;
+            std::fs::write(&jpath, buf)?;
+            Some(jpath)
+        } else {
+            None
+        };
+
+        let ffmpeg_wtr = FfmpegWriter::new(&mp4_filename, ffmpeg_codec_args, rate)?;
+        let mp4_cfg = Mp4RecordingConfig {
+            codec: Mp4Codec::H264RawStream,
+            max_framerate: RecordingFrameRate::Unlimited,
+            h264_metadata,
+        };
+
+        let out_fd = std::fs::File::create(&srt_file_path)?;
+        let swtr = srt_writer::BufferingSrtFrameWriter::new(Box::new(out_fd));
+        Ok(Self {
+            mp4_filename: mp4_filename.to_string(),
+            ffmpeg_wtr,
+            mp4_cfg,
+            srt_file_path,
+            swtr,
+            json_file_path,
+        })
+    }
+
+    /// Write a frame and timestamp.
+    pub fn write_frame<F, TS>(
+        &mut self,
+        frame: &dyn formats::iter::HasRowChunksExact<F>,
+        timestamp: TS,
+    ) -> Result<()>
+    where
+        F: formats::pixel_format::PixelFormat,
+        TS: Into<DateTime<Local>>,
+    {
+        let timestamp = timestamp.into();
+
+        let mp4_pts = self
+            .ffmpeg_wtr
+            .write_frame(frame)
+            .map_err(Error::FfmpegWriter)?;
+
+        let msg = SrtMsg { timestamp };
+        let msg = serde_json::to_string(&msg).unwrap();
+        self.swtr.add_frame(mp4_pts, msg)?;
+        self.swtr.flush()?;
+
+        Ok(())
+    }
+
+    pub fn close(self) -> Result<()> {
+        // finish with ffmpeg and finish writing SRT
+        self.ffmpeg_wtr.close()?;
+        self.swtr.close()?;
+        tracing::debug!("Done creating original .mp4 and .srt files.");
+
+        // Create reader for h264 data from .mp4 and timestamps from .srt.
+        let timestamp_source = frame_source::TimestampSource::SrtFile;
+        let do_decode_h264 = false;
+        tracing::debug!("Opening original .mp4 and .srt files for reading.");
+        let mut frame_src = frame_source::mp4_source::from_path_with_timestamp_source(
+            &self.mp4_filename,
+            do_decode_h264,
+            timestamp_source,
+            Some(PathBuf::from(&self.srt_file_path)),
+        )?;
+        let frame0_time = frame_src.frame0_time().unwrap();
+
+        // Create new .mp4 file, also with original h264 metadata.
+        let fname2 = format!("{}-rewritten.mp4", self.mp4_filename);
+        tracing::debug!(
+            "Copying original .mp4 file into new .mp4 files with timestamps and metadata."
+        );
+        let fd = std::fs::File::create(&fname2)?;
+        let mut new_mp4 = mp4_writer::Mp4Writer::new(fd, self.mp4_cfg, None)?;
+        let h264_src = frame_src.as_seekable_h264_source();
+        new_mp4.set_first_sps_pps(h264_src.first_sps(), h264_src.first_pps());
+
+        let insert_precision_timestamp = true;
+        let width = frame_src.width();
+        let height = frame_src.height();
+
+        let mut count = 0;
+        for frame in frame_src.iter() {
+            let frame = frame?;
+            let timestamp = frame0_time + frame.timestamp().unwrap_duration();
+            let data = match frame.image() {
+                frame_source::ImageData::EncodedH264(data) => &data.data,
+                _ => {
+                    return Err(Error::SourceIsNotH264);
+                }
+            };
+            new_mp4.write_h264_buf(
+                &data,
+                width,
+                height,
+                timestamp,
+                frame0_time,
+                insert_precision_timestamp,
+            )?;
+            count += 1;
+        }
+
+        new_mp4.finish()?;
+        tracing::debug!("Finished writing new .mp4 file with {count} frames.");
+
+        tracing::debug!(
+            "Renaming new .mp4 file to original .mp4 \
+            name, thereby deleting original."
+        );
+        std::fs::rename(fname2, self.mp4_filename)?;
+
+        // Remove no longer need .srt and .json files.
+        std::fs::remove_file(&self.srt_file_path)?;
+        if let Some(jpath) = self.json_file_path {
+            std::fs::remove_file(jpath)?;
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use formats::{pixel_format::RGB8, ImageBuffer, ImageBufferRef, ImageData, Stride};
+
+    use test_log::test;
+
+    /// Image data with a statically typed, strided pixel format.
+    #[derive(Clone)]
+    pub struct MyFrame<F> {
+        pub width: u32,
+        pub height: u32,
+        pub stride: usize,
+        pub image_data: Vec<u8>,
+        pub pixel_format: std::marker::PhantomData<F>,
+    }
+
+    impl<F> ImageData<F> for MyFrame<F> {
+        fn width(&self) -> u32 {
+            self.width
+        }
+        fn height(&self) -> u32 {
+            self.height
+        }
+        fn buffer_ref(&self) -> ImageBufferRef<'_, F> {
+            ImageBufferRef::new(&self.image_data)
+        }
+        fn buffer(self) -> ImageBuffer<F> {
+            ImageBuffer::new(self.image_data)
+        }
+    }
+    impl<F> Stride for MyFrame<F> {
+        fn stride(&self) -> usize {
+            self.stride
+        }
+    }
+
+    #[test]
+    fn test_ffmpeg_rewriter() -> Result<()> {
+        let tempdir = tempfile::tempdir()?;
+        let mp4_fname = tempdir.path().join("out.mp4");
+
+        // let mp4_fname = "out.mp4";
+
+        let timestamp_micros: i64 = 1662921288_000_000; // Sun, 11 Sep 2022 18:34:48 UTC
+
+        let mut timestamps = vec![
+            DateTime::from_timestamp_micros(timestamp_micros).unwrap(),
+            DateTime::from_timestamp_micros(timestamp_micros + 1).unwrap(),
+            DateTime::from_timestamp_micros(timestamp_micros + 100).unwrap(),
+        ];
+
+        for delta in 1..10 {
+            let micros = delta * 10_000;
+            timestamps.push(DateTime::from_timestamp_micros(timestamp_micros + micros).unwrap());
+        }
+
+        tracing::debug!("Encoding {} frames", timestamps.len());
+
+        let w = 640;
+        let h = 480;
+        {
+            // let ffmpeg_codec_args = ffmpeg_writer::platform_hardware_encoder()?;
+            let ffmpeg_codec_args = Default::default();
+
+            let rate = None;
+            let h264_metadata = None;
+            let mut wtr = FfmpegReWriter::new(&mp4_fname, ffmpeg_codec_args, rate, h264_metadata)?;
+
+            for (i, ts) in timestamps.iter().enumerate() {
+                let value = (i % 255) as u8;
+                let frame: MyFrame<RGB8> = MyFrame {
+                    width: w,
+                    height: h,
+                    stride: w as usize * 3,
+                    image_data: vec![value; w as usize * h as usize * 3],
+                    pixel_format: std::marker::PhantomData,
+                };
+                wtr.write_frame(&frame, *ts)?;
+            }
+            wtr.close()?;
+        }
+
+        let do_decode_h264 = false;
+        let timestamp_source = frame_source::TimestampSource::MispMicrosectime;
+        let mut frame_src = frame_source::from_path_with_timestamp_source(
+            &mp4_fname,
+            do_decode_h264,
+            timestamp_source,
+        )?;
+
+        let frame0_time = frame_src.frame0_time().unwrap();
+        assert_eq!(frame0_time, timestamps[0]);
+
+        assert_eq!(frame_src.width(), w as u32);
+        assert_eq!(frame_src.height(), h as u32);
+
+        let mut count = 0;
+        for (i, frame) in frame_src.iter().enumerate() {
+            let frame = frame?;
+            let timestamp = frame0_time + frame.timestamp().unwrap_duration();
+            assert_eq!(timestamps[i], timestamp);
+            count += 1;
+        }
+        assert_eq!(count, timestamps.len());
+
+        Ok(())
+    }
+}
