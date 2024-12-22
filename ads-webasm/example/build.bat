1e6cfb0aebe9cc206a2236b82cf8427dd21a132c
Andrew Straw <strawman@astraw.com>
2024-12-22 06:25:50 +0100

    ads-webasm-example: move crate

--- ads-webasm/example/build.bat
+++ ads-webasm/example/build.bat
@@ -2,4 +2,4 @@ wasm-pack build --target web --dev --features ads-webasm/obj
 
 mkdir pkg
 copy static\index.html pkg
-grass -I ../ads-webasm/scss static/ads-webasm-example.scss pkg/style.css
+grass -I ../scss static/ads-webasm-example.scss pkg/style.css
--- ads-webasm/example/build.sh
+++ ads-webasm/example/build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 wasm-pack build --target web --dev --features ads-webasm/obj
 
 cp static/index.html pkg/index.html
-grass -I ../ads-webasm/scss static/ads-webasm-example.scss pkg/style.css
+grass -I ../scss static/ads-webasm-example.scss pkg/style.css
 
 echo "Build OK. Now run with:\n"
 echo "    microserver --port 8000 --no-spa pkg"
