09708cade19b7ff3a0e5b0c40d8ae1f1d41dba8b
Andrew Straw <strawman@astraw.com>
2024-12-22 04:28:42 +0100

    braidz-viewer: no more need for unstable api

--- braidz-viewer/build.sh
+++ braidz-viewer/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -o errexit
+set -euo pipefail
 
 DEPLOY_DIR=deploy
 
 # Install wasm-pack from here https://rustwasm.github.io/wasm-pack/installer/
-RUSTFLAGS=--cfg=web_sys_unstable_apis wasm-pack build --target web
+wasm-pack build --target web
 mkdir -p $DEPLOY_DIR
 rm -rf $DEPLOY_DIR/*
 cp -pr pkg/* $DEPLOY_DIR/
