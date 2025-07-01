ba7f3afef7e68fb7f146ae0ab1738a0b7af3ee33
Andrew Straw <strawman@astraw.com>
2025-07-01 07:14:00 +0200

    strand-cam-enum-iter: add docstrings

--- utils/strand-cam-enum-iter/src/lib.rs
+++ utils/strand-cam-enum-iter/src/lib.rs
@@ -1,11 +1,17 @@
-/// Get all variants.
+//! A small utility crate to provide [`EnumIter`] trait for iterating over enums
+//! in the [Strand Camera](https://strawlab.org/strand-cam) ecosystem.
+
+/// Allows collecting all variants of an enum.
 ///
-/// See also the IntoEnumIterator trait of the `strum` crate for a
-/// version which can be automatically derived.
+/// See also the
+/// [`IntoEnumIterator`](https://docs.rs/strum/0.27.1/strum/trait.IntoEnumIterator.html)
+/// trait of the [`strum`](https://docs.rs/strum) crate for a version which can
+/// be automatically derived.
 pub trait EnumIter
 where
     Self: Sized,
 {
+    /// Returns a vector containing all variants of the enum.
     fn variants() -> Vec<Self>;
 }
 
