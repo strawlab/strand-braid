0f97dace47f2a0a0ae745db35b6b4af2e24e2f4f
Andrew Straw <strawman@astraw.com>
2025-07-05 18:18:35 +0200

    strand-cam-enum-iter: Fix docstring
    fixup 07c1b52017a4049bec97418d70812239ace3a733

--- utils/strand-cam-enum-iter/src/lib.rs
+++ utils/strand-cam-enum-iter/src/lib.rs
@@ -1,13 +1,6 @@
 //! A small utility crate to provide [`EnumIter`] trait for iterating over enums
 //! in the [Strand Camera](https://strawlab.org/strand-cam) ecosystem.
 
-/// Allows collecting all variants of an enum.
-///
-/// See also the
-/// [`IntoEnumIterator`](https://docs.rs/strum/0.27.1/strum/trait.IntoEnumIterator.html)
-/// trait of the [`strum`](https://docs.rs/strum) crate for a version which can
-/// be automatically derived.
-
 // Copyright 2020-2025 Andrew D. Straw.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
@@ -15,6 +8,12 @@
 // or http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
+/// Allows collecting all variants of an enum.
+///
+/// See also the
+/// [`IntoEnumIterator`](https://docs.rs/strum/0.27.1/strum/trait.IntoEnumIterator.html)
+/// trait of the [`strum`](https://docs.rs/strum) crate for a version which can
+/// be automatically derived.
 pub trait EnumIter
 where
     Self: Sized,
