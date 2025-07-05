07c1b52017a4049bec97418d70812239ace3a733
Andrew Straw <strawman@astraw.com>
2025-07-05 17:52:43 +0200

    strand-cam-enum-iter: add copyright to lib.rs file

--- utils/strand-cam-enum-iter/src/lib.rs
+++ utils/strand-cam-enum-iter/src/lib.rs
@@ -7,6 +7,14 @@
 /// [`IntoEnumIterator`](https://docs.rs/strum/0.27.1/strum/trait.IntoEnumIterator.html)
 /// trait of the [`strum`](https://docs.rs/strum) crate for a version which can
 /// be automatically derived.
+
+// Copyright 2020-2025 Andrew D. Straw.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT
+// or http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 pub trait EnumIter
 where
     Self: Sized,
