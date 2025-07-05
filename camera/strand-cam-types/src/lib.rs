1e6ffb9869ce40cf6d34bdc49ef3ece806b47525
Andrew Straw <strawman@astraw.com>
2025-07-05 18:00:34 +0200

    strand-cam-types: preparing for release

--- camera/strand-cam-types/src/lib.rs
+++ camera/strand-cam-types/src/lib.rs
@@ -1,6 +1,11 @@
-extern crate strand_cam_enum_iter;
+// Copyright 2020-2025 Andrew D. Straw.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT
+// or http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use strand_cam_enum_iter::EnumIter;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, Default)]
