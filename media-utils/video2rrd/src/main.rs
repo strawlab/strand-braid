bd138ce53f1465e438c884a4cbe8f39eb50c2c58
Andrew Straw <strawman@astraw.com>
2025-08-16 08:14:27 +0200

    video2rrd: do not reencode video if not necessary
    This makes video2rrd vastly faster (unless
    undistortion is needed).

--- media-utils/video2rrd/src/main.rs
+++ media-utils/video2rrd/src/main.rs
@@ -59,18 +59,14 @@ struct Opt {
 
 fn to_rr_image(
     im: ImageData,
-    undist_cache: Option<&undistort_image::UndistortionCache>,
+    undist_cache: &undistort_image::UndistortionCache,
 ) -> eyre::Result<re_types::archetypes::EncodedImage> {
     let decoded = match im {
         ImageData::Decoded(decoded) => decoded,
         _ => eyre::bail!("image not decoded"),
     };
 
-    let to_save = if let Some(undist_cache) = undist_cache {
-        undistort_image::undistort_image(decoded.borrow(), &undist_cache)?
-    } else {
-        decoded
-    };
+    let to_save = { undistort_image::undistort_image(decoded.borrow(), &undist_cache)? };
 
     // jpeg compression TODO: give option to save uncompressed?
     let contents = to_save
@@ -109,14 +105,16 @@ impl DeleteIfDropped {
     }
 }
 
+fn get_timestamp(ts: &chrono::DateTime<chrono::FixedOffset>) -> i64 {
+    ts.timestamp_nanos_opt().unwrap()
+}
+
 fn main() -> eyre::Result<()> {
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "info");
     }
     env_tracing_logger::init();
     let opt = Opt::parse();
-
-    let mut src = frame_source::FrameSourceBuilder::new(&opt.input).build_source()?;
 
     let entity_path = if let Some(p) = opt.entity_path.as_ref() {
         p.clone()
@@ -151,6 +149,13 @@ fn main() -> eyre::Result<()> {
     } else {
         None
     };
+
+    // We need the images iff we will undistort them.
+    let do_decode_h264 = undist_cache.is_some();
+
+    let mut src = frame_source::FrameSourceBuilder::new(&opt.input)
+        .do_decode_h264(do_decode_h264)
+        .build_source()?;
 
     tracing::info!("Frame size: {}x{}", src.width(), src.height());
 
@@ -224,6 +229,7 @@ fn main() -> eyre::Result<()> {
         None
     };
 
+    let mut absolute_timestamps = Vec::new();
     for (fno, frame) in src_iter.enumerate() {
         let frame = frame?;
         let pts = if let Some(forced_framerate) = opt.framerate.as_ref() {
@@ -242,6 +248,7 @@ fn main() -> eyre::Result<()> {
         };
 
         let stamp_chrono = start_time + pts;
+        absolute_timestamps.push(stamp_chrono);
 
         if let Some(first_time) = opt.exclude_before {
             if stamp_chrono < first_time {
@@ -255,14 +262,18 @@ fn main() -> eyre::Result<()> {
             }
         }
 
-        {
-            let time: std::time::SystemTime = stamp_chrono.into();
-            let time: re_sdk::TimeCell = time.try_into().map_err(|_| eyre::eyre!("ts fail"))?;
-            rec.set_time("wall_clock", time);
-        }
-        let image = to_rr_image(frame.into_image(), undist_cache.as_ref())?;
+        if let Some(undist_cache) = undist_cache.as_ref() {
+            {
+                let time: std::time::SystemTime = stamp_chrono.into();
+                let time: re_sdk::TimeCell = time.try_into().map_err(|_| eyre::eyre!("ts fail"))?;
+                rec.set_time("wall_clock", time);
+            }
 
-        rec.log(entity_path.as_str(), &image)?;
+            let image = to_rr_image(frame.into_image(), undist_cache)?;
+
+            rec.log(entity_path.as_str(), &image)?;
+        }
+
         if let Some(pb) = &pb {
             // Increment the counter.
             pb.inc(1);
@@ -270,6 +281,28 @@ fn main() -> eyre::Result<()> {
     }
     if let Some(pb) = pb {
         pb.finish_and_clear();
+    }
+
+    if undist_cache.is_none() {
+        let video_asset = re_types::archetypes::AssetVideo::from_file_path(&opt.input).unwrap();
+        rec.log_static(entity_path.as_str(), &video_asset)?;
+        let frame_timestamps_nanos =
+            re_types::archetypes::AssetVideo::read_frame_timestamps_nanos(&video_asset)?;
+        let video_timestamps_nanos = frame_timestamps_nanos
+            .iter()
+            .copied()
+            .map(re_types::components::VideoTimestamp::from_nanos)
+            .collect::<Vec<_>>();
+        let absolute_nanos: Vec<i64> = absolute_timestamps.iter().map(get_timestamp).collect();
+        let time_column =
+            re_chunk::TimeColumn::new_timestamp_nanos_since_epoch("wall_clock", absolute_nanos);
+        rec.send_columns(
+            entity_path,
+            [time_column],
+            re_types::archetypes::VideoFrameReference::update_fields()
+                .with_many_timestamp(video_timestamps_nanos)
+                .columns_of_unit_batches()?,
+        )?;
     }
 
     if let Some(delete_if_dropped) = delete_if_dropped {
