92c40c5678e51b9f08a8fd316323de273f1989e6
Andrew Straw <strawman@astraw.com>
2026-01-30 13:01:14 +0100

    ci: use sandybridge target from .cargo/config.toml

--- strand-cam/strand-cam-vimba/build-plain.sh
+++ strand-cam/strand-cam-vimba/build-plain.sh
@@ -4,6 +4,6 @@ set -o errexit
 # Prerequisite: ../yew_frontend/dist is built. Do this by "build.sh" in yew_frontend.
 
 PKG_CONFIG_PATH=/opt/libvpx/libvpx-1.8.0/lib/pkgconfig \
-RUSTFLAGS="$RUSTFLAGS -C target-cpu=sandybridge -C codegen-units=1" \
+RUSTFLAGS="$RUSTFLAGS -C codegen-units=1" \
 NUM_JOBS=2 \
 cargo build --features "strand-cam/imtrack-absdiff strand-cam/bundle_files" --release
--- strand-cam/strand-cam-vimba/build.sh
+++ strand-cam/strand-cam-vimba/build.sh
@@ -9,6 +9,6 @@ source /opt/intel/bin/compilervars.sh -arch intel64 -platform linux && \
 OPENCV_STATIC=1 \
 PKG_CONFIG_PATH=/opt/opencv-4.5.5-static/lib/pkgconfig \
 IPP_STATIC=1 \
-RUSTFLAGS="$RUSTFLAGS -C target-cpu=sandybridge -C codegen-units=1" \
+RUSTFLAGS="$RUSTFLAGS -C codegen-units=1" \
 NUM_JOBS=2 \
 cargo build --features "strand-cam/flydra_feat_detect strand-cam/use_ipp strand-cam/imtrack-absdiff strand-cam/bundle_files ipp-sys/2019 strand-cam/checkercal strand-cam/fiducial imops/simd" --release
