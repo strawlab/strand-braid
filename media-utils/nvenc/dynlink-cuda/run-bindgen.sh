5765f74cfa61c2b9a9fcff8cef1f3a1ad51fe5d5
Andrew Straw <strawman@astraw.com>
2026-06-06 10:30:59 +0200

    dynlink-cuda: only include required types

--- media-utils/nvenc/dynlink-cuda/run-bindgen.sh
+++ media-utils/nvenc/dynlink-cuda/run-bindgen.sh
@@ -7,5 +7,12 @@ bindgen --version | grep -Eq '^bindgen 0\.72\.[0-9]+$'
 bindgen /usr/local/cuda-10.0/include/cuda.h \
     --default-enum-style moduleconsts \
     --distrust-clang-mangling \
+    --allowlist-function "cu.*" \
+    --allowlist-type "CU.*" \
+    --allowlist-type "cu.*" \
+    --allowlist-type "cuda.*" \
+    --allowlist-type "CUDA.*" \
+    --allowlist-var "CU.*" \
+    --allowlist-var "cuda.*" \
     --raw-line '#![allow(dead_code,non_upper_case_globals,non_camel_case_types,non_snake_case,unused_imports)]' \
     -o src/ffi.rs
